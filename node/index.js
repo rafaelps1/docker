9fa53747d8ddd94f4b03e1207bc58fffd611172c
Rafael Pereira <rafaelps@dcomp.ufs.br>
2023-05-02 19:46:43 -0300

    Connet db with app node

--- node/index.js
+++ node/index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) VALUES('Rafael Pereira')`
+connection.query(sql)
+connection.end()
 
 app.get('/', (req, res) =>{
-    res.send('<h1>Hello Man!!</h1>')
+    res.send('<h1>Hello, Man!!</h1>')
 })
 
 app.listen(port, ()=> {
